7dff20b52cb524e6c2e0bfda3343d2ca603e3f2e
Tera Coder <TerableCoder@gmail.com>
2020-06-20 00:03:29 -0400

    Fully Working

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TerableReminder
+
+## Commands:
+### `rem add seconds msg`
+- Add reminder, Example: `rem add 600 Go pick-up the pizza`
+### `rem list`
+- List remaining reminders
--- index.js
+++ index.js
@@ -10,11 +10,13 @@ module.exports = function TerableReminder(mod) {
 			command.message(`rem add seconds msg`);
 			command.message(`rem list`);
 		},
-		add(seconds, msg){
+		add(seconds, ...msg){
 			const tmpCounter = counter;
+			msg = msg.join(' ').toString();
 			timeouts.push({counter: counter++, msg: msg});
 			setTimeout(() => {
 				notifier.message(msg);
+				command.message(msg);
 				let index = timeouts.findIndex(element => element.counter == tmpCounter);
 				timeouts.splice(index, 1);
 			}, seconds*1000);
